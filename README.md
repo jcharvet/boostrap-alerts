4730fa544ec47ea4bb4ea7678193264edaefef7c
Eliphas <charvet.jerome@gmail.com>
2015-06-27 10:31:37 +0100

    merge

---
+++ README.md
@@ -0,0 +1,21 @@
+# boostrap-alerts
+A simple Meteor JS boostrap alerts
+
+based on bojicas:bootstrap-alerts
+
+
+USAGE
+meteor add jcharvet:bootstrap-alerts
+
+
+    Add {{> BootstrapAlerts}} to your template / layout.
+    Call alerts with Alerts.set('content', 'type'), where type can take following values:
+        success
+        info
+        warning
+        danger
+
+Note that you can call Alerts.set('content'); (without a second parameter) in order to get an alert of danger type.
+
+LICENSE
+MIT
---
+++ package.js
@@ -0,0 +1,37 @@
+Package.describe({
+  name: 'jcharvet:bootstrap-alerts',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Display bootstrap alerts',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/jcharvet/boostrap-alerts.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('mongo');
+  api.use([
+    'twbs:bootstrap@3.3.1_2',
+    'templating'
+  ], 'client');
+
+  api.addFiles([
+    'bootstrap-alerts.css',
+    'bootstrap-alerts.html',
+    'bootstrap-alerts.js'
+  ], 'client');
+
+  if (api.export) {
+    api.export('Alerts');
+  }
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jcharvet:bootstrap-alerts');
+  api.addFiles('bootstrap-alerts-tests.js');
+});
