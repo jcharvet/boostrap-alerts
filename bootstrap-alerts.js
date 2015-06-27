2236518623f1d3849bdc0c6d91bde751e441746f
Eliphas <charvet.jerome@gmail.com>
2015-06-27 09:38:02 +0100

    initial push

---
+++ bootstrap-alerts.js
@@ -0,0 +1,22 @@
+Alerts = {
+  collection: new Mongo.Collection(null),
+  set: function (message, type) {
+    Alerts.collection.insert({
+      message: message,
+      type: type || 'danger'
+    });
+  }
+};
+
+Template.BootstrapAlerts.helpers({
+  alerts: function () {
+    return Alerts.collection.find();
+  }
+});
+
+Template.BootstrapAlert.rendered = function () {
+  var currentAlert = this.data;
+  Meteor.setTimeout(function () {
+    Alerts.collection.remove(currentAlert._id);
+  }, 3000);
+};
